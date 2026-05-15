1d7c7147eeb9395aa4f8749be9ee50fd44744b28
violrend <95243050+violrend@users.noreply.github.com>
2026-05-15 11:37:09 +0300

    Refactor image handling in MainTabView

--- SnapShe/Views/MainTabView.swift
+++ SnapShe/Views/MainTabView.swift
@@ -1,18 +1,23 @@
 import SwiftUI
 
+// Wrapper: UIImage'ı Identifiable yapıyoruz ki sheet(item:) kullanabilelim
+private struct IdentifiableImage: Identifiable {
+    let id = UUID()
+    let image: UIImage
+}
+
 struct MainTabView: View {
-    @State private var showVisualSearch = false
-    @State private var pendingImage: UIImage? = nil
+    @State private var pendingImage: IdentifiableImage? = nil
 
     var body: some View {
         HomeView(onVisualSearch: { image in
-            pendingImage = image
-            showVisualSearch = true
+            pendingImage = IdentifiableImage(image: image)
         })
-        .sheet(isPresented: $showVisualSearch) {
-            if let img = pendingImage {
-                VisualSearchView(initialImage: img)
-            }
+        .sheet(item: $pendingImage) { wrapper in
+            VisualSearchView(initialImage: wrapper.image)
+                .onDisappear {
+                    NotificationCenter.default.post(name: .feedNeedsRefresh, object: nil)
+                }
         }
     }
 }
